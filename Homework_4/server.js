dd463da4c986149e23451a537faaa1d1b6ed0e23
Ngo The Dong <ngothedong97@gmail.com>
2018-12-20 23:17:16 +0700

    Cv

---
+++ Homework_4/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/asset'));
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + "/cvNgoTheDong.html")
+});
+app.listen(6969, (err) => {
+    if(err)
+    console.log("Something's wrong");
+    else console.log("Server start sucess");
+});
